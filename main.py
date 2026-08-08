90ca5b0cc40b09d51edf276ec4a29d18f3e67322
ket-chaudhari <ketakichaudhari3@gmail.com>
2026-08-08 21:29:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,20 +5,24 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {
-        "message": "RAG API is running"
-    }
+    return {"message": "RAG API is running"}
 
 
 @app.get("/health")
 def health():
-    return {
-        "status": "healthy"
-    }
+    return {"status": "healthy"}
 
 
 @app.get("/test")
 def test():
-    return {
-        "message": "Test route working"
-    }
+    return {"message": "Test route working"}
+
+
+@app.post("/upload")
+def upload():
+    return {"message": "Upload route working"}
+
+
+@app.post("/ask")
+def ask():
+    return {"message": "Ask route working"}
